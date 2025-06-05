6a5ae437f85687097bc8339eead0f702691daf31
iemsel <jong0394@hz.nl>
2025-06-05 18:27:34 +0200

    log in fix

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
 
-// Public route: login
+require __DIR__.'/auth.php';
+
+// Redirect root to login
 Route::get('/', function () {
-    return view('login');
-})->name('login');
+    return redirect()->route('login');
+});
 
-// Public route: about (but only if you want it public)
-Route::view('/about', 'about')->name('about');
+// Everything else should be protected
+Route::middleware(['auth', 'verified'])->group(function () {
+    // The about/dashboard page
+    Route::view('/about', 'about')->name('dashboard');
 
-// Authenticated routes
-Route::middleware(['auth'])->group(function () {
+    // Home route
     Route::get('/home', [TaskController::class, 'index'])->name('home');
 
+    // Resource routes
     Route::resource('tasks', TaskController::class)->except(['index']);
     Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
     Route::resource('posts', PostController::class);
     Route::resource('projects', ProjectController::class);
 
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
